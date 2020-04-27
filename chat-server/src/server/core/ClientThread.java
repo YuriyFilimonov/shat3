44dfe4a0a6d0d8eed2f49185f9d064f16fa43913
Yury Filimonov <yuriy.filimonov@gmail.com>
2020-04-27 12:02:08 +0300

    Внес в строку сообщения Nickname отправителя. Затем, в общей рассылке выловил обратное сообщение отправителю и отформатировал строку, заменив Nickname на "from me".

---
+++ chat-server/src/server/core/ClientThread.java
@@ -0,0 +1,43 @@
+package server.core;
+
+import common.Library;
+import network.SocketThread;
+import network.SocketThreadListener;
+
+import java.net.Socket;
+
+public class ClientThread extends SocketThread {
+
+    private String nickname;
+    private boolean isAuthorized;
+
+    public ClientThread(SocketThreadListener listener, String name, Socket socket) {
+        super(listener, name, socket);
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public boolean isAuthorized() {
+        return isAuthorized;
+    }
+
+    public void authAccept(String nickname) {
+        isAuthorized = true;
+        this.nickname = nickname;
+        sendMessage(Library.getAuthAccept(nickname));
+    }
+
+    public void authFail() {
+        sendMessage(Library.getAuthDenied());
+        close();
+    }
+
+    public void msgFormatError(String msg) {
+        sendMessage(Library.getMsgFormatError(msg));
+        close();
+    }
+
+
+}
